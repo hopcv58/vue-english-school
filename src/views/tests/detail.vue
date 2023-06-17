7e481cdc1d0aa382c980e9fb0a9bf05bfbf6366a
HOPCV\andui <hopcv58@gmail.com>
2023-06-17 20:44:45 +0700

    update

--- src/views/tests/detail.vue
+++ src/views/tests/detail.vue
@@ -1,136 +1,96 @@
 <template>
   <div class="content position-r w-100" style="background: #F2F2F2; min-height: calc(100vh - 128px)">
-    <div class="container-fluid">
-      <div class="row justify-content-center">
-        <Title>
-          Thêm bài kiểm tra
-        </Title>
-        <modal :show.sync="addQuestionModal.show">
-          <h6 slot="header" class="modal-title" id="modal-title-default">Thêm câu hỏi vào đề thi</h6>
-
-          <select v-model="addQuestionModal.selectingTagId" class="form-control col-6" @change="getQuestionsForModal">
-            <option value="">Chọn tag</option>
-            <option v-for="tag in tagList" :value="tag.id" :key="tag.id">{{ tag.name }}</option>
-          </select>
-
-          <table v-if="addQuestionModal.questions.length" class="table table-striped">
-            <thead>
-            <tr>
-              <th scope="col">Chọn</th>
-              <th scope="col">Nội dung</th>
-              <th scope="col">Tags</th>
-            </tr>
-            </thead>
-            <tbody>
-            <tr v-for="question in addQuestionModal.questions" :key="question.id">
-              <td>
-                <input type="checkbox" :value="question.id" v-model="addQuestionModal.selectingQuestionIds">
-              </td>
-              <td>{{ question.content }}</td>
-              <td>
-                 <span v-for="tag in question.tagList" class="badge badge-primary mr-1" :key="tag.id">{{ tag.name }}</span>
-              </td>
-            </tr>
-            </tbody>
-          </table>
-
-          <template slot="footer">
-            <base-button type="primary" @click="saveModalChanges">Save changes</base-button>
-            <base-button type="link" class="ml-auto" @click="discardModalChanges">
-              Close
-            </base-button>
-          </template>
-        </modal>
-        <section class="section section-lg pt-lg-0 w-100" style="margin-top: 200px">
-          <div class="py-5 bg-secondary">
-            <div class="container">
-              <form @submit.prevent="storeTest">
-                <div class="form-row">
-                  <div class="form-group col-md-12">
-                    <label for="content">Tên bài kiểm tra</label>
-                    <input v-model="name" required class="form-control"/>
+    <div class="container-fluid" style="min-height: calc(100vh - 128px)">
+      <div class="row justify-content-center" style="margin-top: 68px;min-height: calc(100vh - 128px)">
+        <div class="col-3 _col-1 position-r">
+          <div class="position-a" style="left: 0; top: 30%; width: 80%; max-width: 180px">
+            <img src="https://learn.mochidemy.com/image/213202355_4534422609904130_3896387388468451408_n_1.png"
+                 style="width: 100%">
+          </div>
+        </div>
+        <div class="col-6 _col-2 main-center" style="padding-bottom: 10px; min-height: 616px;">
+          <div class="div-review" style="">
+            <div class="row div_process" style="margin: 0; padding: 30px 0 10px">
+              <div class="col-2 text-center">
+                <img @click="endTestEarly" src="https://learn.mochidemy.com/svg/close_game.svg"
+                     style="width: 25px; cursor: pointer">
+              </div>
+              <div class="col-9">
+                <div class="process_bar" style="">
+                  <div class="process-element" data-content="5" id="process-element"
+                       :style="{'width': this.progress + '%'}">
+                    <img src="https://learn.mochidemy.com/image/9362859030ff2f1748657ae47ef40370.png" alt="">
                   </div>
                 </div>
-                <div class="form-row">
-                  <div class="form-group col-md-12">
-                    <label for="content">Mô tả</label>
-                    <textarea v-model="description" required class="form-control" rows="5"></textarea>
-                  </div>
-                </div>
-                <div class="form-row">
-                  <div class="form-group col-md-12">
-                    <label for="content">Thời gian làm bài</label>
-                    <input v-model="availableTime" required class="form-control" type="number"/>
-                  </div>
-                </div>
-                <div class="form-row">
-                  <div class="form-group col-md-12">
-                    <label for="tags">Tags</label>
-                    <b-form-tags
-                        v-if="tagNameList.length > 0"
-                        id="tags-component-select"
-                        v-model="selectedTags"
-                        class="mb-2"
-                        add-on-change
-                        no-outer-focus
-                    >
-                      <template v-slot="{ tags, inputAttrs, inputHandlers, disabled, removeTag }">
-                        <ul v-if="tags.length > 0" class="list-inline d-inline-block mb-2">
-                          <li v-for="tag in tags" :key="tag" class="list-inline-item">
-                            <b-form-tag
-                                @remove="removeTag(tag)"
-                                :title="tag"
-                                :disabled="disabled"
-                            >{{ tag }}
-                            </b-form-tag>
-                          </li>
-                        </ul>
-                        <b-form-select
-                            v-bind="inputAttrs"
-                            v-on="inputHandlers"
-                            :disabled="disabled || availableTags.length === 0"
-                            :options="availableTags"
-                        >
-                          <template #first>
-                            <!-- This is required to prevent bugs with Safari -->
-                            <option disabled value="">Chọn tag</option>
-                          </template>
-                        </b-form-select>
-                      </template>
-                    </b-form-tags>
-                  </div>
-                </div>
-                <button type="submit" class="btn btn-success">Cập nhật</button>
-                <button type="button" class="btn btn-primary" @click="showModal">Thêm câu hỏi</button>
-              </form>
+              </div>
+            </div>
 
-              <h4 v-if="addQuestionModal.selectedQuestionIds.length" class="mt-5">
-                Các câu hỏi đã chọn
-              </h4>
-              <h4 v-else class="mt-5">
-                Chưa có câu hỏi nào được chọn
-              </h4>
-              <table v-if="addQuestionModal.selectedQuestionIds.length" class="table table-striped">
-                <thead>
-                <tr>
-                  <th scope="col">Nội dung</th>
-                  <th scope="col">Tags</th>
-                </tr>
-                </thead>
-                <tbody>
-                <template v-for="question in questions">
-                  <tr v-if="addQuestionModal.selectedQuestionIds.includes(question.id)">
-                    <td>{{ question.content }}</td>
-                    <td>
-                       <span v-for="tag in question.tagList" class="badge badge-primary mr-1" :key="tag.id">{{ tag.name }}</span>
-                    </td>
-                  </tr>
-                </template>
-                </tbody>
-              </table>
+            <div v-if="totalQuestions" class="learn-game position-r"
+                 style="overflow: hidden auto; padding-bottom: 30px;">
+              <div id="game">
+                <div class="game-learn-2 game-learn-word">
+                  <div class="row justify-content-center position-r">
+                    <div class="col-9">
+                      <div class="game-learn-2 game-learn-word">
+                        <div class="text-center w-100">
+                          <p class="title-game-2" v-html="currentQuestion.content">
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-9">
+                      <div class="box-answer-3">
+                        <div class="text-center list-answer-3">
+                          <p class="mb-0" style="font-size: 15px; font-weight: bold" v-html="currentQuestion.question">
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-8">
+                      <div class="div-answer-game" style="margin-top: 30px">
+                        <div v-for="answerNo in [1,2,3,4]" :key="answerNo" class="bg-answer-item">
+                          <div class="answer-review-item item-game text-center"
+                               :class="{'answer-review-item-success': currentAnswer === answerNo}"
+                               :data-answer="answerNo" @click="setAnswer(answerNo)">
+                            <p class="mb-0" v-html="currentQuestion['answer' + answerNo]">
+                            </p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-        </section>
+          <div class="position-a mochi-hello" style="bottom: 0px; left: 0px;">
+            <img src="https://learn.mochidemy.com/svg/icon-hello.svg" style="width: 100px">
+          </div>
+        </div>
+        <div class="col-3 _col-3 text-center position-r">
+          <div class="position-a" style="right: 0; top: 0; width: 70%; max-width: 150px">
+            <img src="https://learn.mochidemy.com/image/213202355_4534422609904130_3896387388468451408_n_2.png"
+                 style="width: 100%">
+          </div>
+          <div class="position-a" style="right: 0; top: 50%; width: 80%">
+            <img src="https://learn.mochidemy.com/image/213202355_4534422609904130_3896387388468451408_n.png.webp"
+                 style="width: 100%">
+          </div>
+          <div class="result-questions">
+            <h2 class="result-box-header result-box-title">Question List</h2>
+            <div class="result-box-body">
+              <div class="row g-0 result-box-list">
+                <div v-for="(question, index) in questions" :key="index"
+                     class="result-box-span" :class="{
+                  answering: index === currentQuestionNo,
+                  answered: answers[index] !== undefined,
+                }" @click="goToQuestion(index)">
+                  <div class="result-box-number">{{ index + 1 }}</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -140,7 +100,7 @@
 import axios from 'axios'
 import {BFormTags, BFormTag, BFormSelect} from 'bootstrap-vue'
 import Modal from '@/components/Modal.vue'
-import { store } from "@/store";
+import {store} from '@/store'
 
 export default {
   name: 'tests',
@@ -152,140 +112,119 @@ export default {
   },
   data() {
     return {
-      addQuestionModal: {
-        show: false,
-        selectingTagId: null,
-        questions: [],
-        selectingQuestionIds: [],
-        selectedQuestionIds: []
-      },
-      tagList: [],
-      questions: [],
       store,
-      name: '',
-      description: '',
-      availableTime: '',
-      selectedTags: [],
+      questions: [],
+      answers: [],
+      currentQuestionNo: 0,
+      currentAnswer: 0,
+      totalQuestions: 0,
+      totalAnswers: 0,
+      availableTime: 0,
+      passedTime: 0,
     }
   },
   computed: {
-    tagNameList() {
-      return this.tagList.map(tag => tag.name)
+    progress() {
+      if (this.totalQuestions === 0) {
+        return 5
+      }
+      const progress = (this.currentQuestionNo + 1) / this.totalQuestions * 100
+      if (progress > 100) {
+        return 100
+      }
+      return progress < 5 ? 5 : progress
     },
-    availableTags() {
-      return this.tagNameList.filter(tag => !this.selectedTags.includes(tag))
+    currentQuestion() {
+      if (this.totalQuestions === 0) {
+        return {}
+      }
+      return this.questions[this.currentQuestionNo]
+    },
+    formattedRemainingTime() {
+      const remainingTime = this.availableTime - this.passedTime
+      if (remainingTime <= 0) {
+        return '00:00'
+      }
+      const minutes = Math.floor(remainingTime / 60)
+      const seconds = remainingTime % 60
+      return `${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`
     },
   },
   async created() {
-    await this.getTags()
     await this.getQuestions()
-    await axios.get('http://localhost:8080/quiz/api/tests/' + this.$route.params.id)
+    await axios.get('http://localhost:8080/quiz/api/tests/' + this.$route.params.id,
+        {
+          headers: {
+            'Authorization': `Bearer ${store.token}`
+          }
+        })
         .then(response => {
-          this.name = response.data.data.name
-          this.description = response.data.data.description
-          this.availableTime = response.data.data.availableTime
-          this.selectedTags = response.data.data.tagList ? response.data.data.tagList.map(tag => tag.name) : []
-          this.addQuestionModal.selectedQuestionIds = response.data.data.questionList ? response.data.data.questionList.map(question => question.id) : []
+          this.questions = response.data.data.questionList
+          this.availableTime = response.data.data.availableTime * 60 // convert to seconds
+          // set Interval to count down. When time is up, submit test and clear interval
+          this.interval = setInterval(() => {
+            this.passedTime++
+            if (this.passedTime >= this.availableTime) {
+              this.submit()
+              clearInterval(this.interval)
+            }
+          }, 1000)
         })
   },
   methods: {
-    showModal() {
-      this.addQuestionModal.show = true
-      this.addQuestionModal.selectingTagId = null
-      this.addQuestionModal.questions = []
-      this.addQuestionModal.selectingQuestionIds = this.addQuestionModal.selectedQuestionIds
-    },
-    discardModalChanges() {
-      this.addQuestionModal.show = false
-      this.addQuestionModal.selectingTagId = null
-      this.addQuestionModal.questions = []
-      this.addQuestionModal.selectingQuestionIds = this.addQuestionModal.selectedQuestionIds
-    },
-    saveModalChanges() {
-      this.addQuestionModal.show = false
-      this.addQuestionModal.selectingTagId = null
-      this.addQuestionModal.questions = []
-      this.addQuestionModal.selectedQuestionIds = this.addQuestionModal.selectingQuestionIds
-      this.getQuestions()
-    },
-    async storeTest() {
-      if (!this.name || !this.availableTime) {
-        store.displayError('Vui lòng nhập đầy đủ thông tin')
-        return
-      }
-
-      const questionIds = []
-      for (let questionId of this.addQuestionModal.selectedQuestionIds) {
-        questionIds.push({id: questionId})
-      }
-
-      const tagIds = []
-      for (let tagName of this.selectedTags) {
-        const tag = this.tagList.find(tag => tag.name === tagName)
-        tagIds.push({id: tag.id})
-      }
-
-      await axios.put('http://localhost:8080/quiz/api/tests/' + this.$route.params.id, {
-        id: this.$route.params.id,
-        name: this.name,
-        description: this.description,
-        availableTime: this.availableTime,
-        questionList: questionIds,
-        tagList: tagIds
-      }, {
-        headers: {
-          'Authorization': `Bearer ${store.token}`
-        }
-      })
-          .then(res => {
-            store.displaySuccess('Cập nhật thành công')
-          })
-          .catch(err => {
-            store.displayError('Có lỗi xảy ra. Vui lòng thử lại')
-          })
-    },
-    async getQuestionsForModal() {
-      if (!this.addQuestionModal.selectingTagId) {
-        this.addQuestionModal.questions = []
-        return
-      }
-      await axios.get('http://localhost:8080/quiz/api/questions?pageSize=100000&pageNo=0&tagId=' + this.addQuestionModal.selectingTagId)
-          .then(res => {
-            this.addQuestionModal.questions = res.data.data.items
-          })
-          .catch(err => {
-            store.displayError('Có lỗi xảy ra. Vui lòng thử lại')
-          })
-    },
-    async getTags() {
-      await axios.get('http://localhost:8080/quiz/api/tags?pageSize=100000&pageNo=0')
-          .then(res => {
-            if (res.data.data.items.length === 0) {
-              return
-            }
-            this.tagList = res.data.data.items
-          })
-          .catch(err => {
-            store.displayError('Có lỗi xảy ra. Vui lòng thử lại')
-          })
+    endTestEarly() {
+      this.$router.push('/tests')
     },
     async getQuestions() {
-      await axios.get('http://localhost:8080/quiz/api/questions?pageSize=100000&pageNo=0')
+      await axios.get('http://localhost:8080/quiz/api/tests/' + this.$route.params.id)
           .then(res => {
-            this.questions = res.data.data.items
+            this.questions = res.data.data.questionList
+            this.totalQuestions = res.data.data.questionList.length
           })
           .catch(err => {
             store.displayError('Có lỗi xảy ra. Vui lòng thử lại')
           })
-    }
+    },
+    setAnswer(answer) {
+      this.currentAnswer = answer
+    },
+    skipQuestion() {
+      if (this.currentQuestionNo < this.totalQuestions - 1) {
+        this.currentQuestionNo++
+        this.currentAnswer = 0
+      }
+    },
+    nextQuestion() {
+      this.answers[this.currentQuestionNo] = this.currentAnswer
+      this.currentAnswer = 0
+      this.currentQuestionNo++
+      this.totalAnswers++
+    },
+    submitTest() {
+      if (this.totalAnswers < this.totalQuestions) {
+        this.store.confirmModal = {
+          show: true,
+          title: 'Bạn chưa hoàn thành bài thi',
+          content: 'Bạn có chắc chắn muốn nộp bài?',
+          onConfirm: this.submit,
+        }
+      } else {
+        this.submit()
+      }
+    },
+    submit() {
+
+    },
+    goToQuestion(index) {
+      if (this.currentAnswer !== 0) {
+        this.answers[this.currentQuestionNo] = this.currentAnswer
+      }
+      this.currentQuestionNo = index
+      this.currentAnswer = this.answers[index] || 0
+    },
+
   }
 }
 </script>
 <style>
-.badge-secondary {
-  background-color: #5e72e4 !important;
-  color: #fff !important;
-  height: 24px !important;
-  align-items: center !important;
-}
 </style>
